5dda7197760d98b3bbee3aaa4f5dd16b5027626f
Tom Emmerson <tom@carefully.codes>
2025-12-09 13:28:08 +0000

    Fix prices not syncing

--- ecommerce_integrations/shopify/doctype/shopify_setting/shopify_setting.js
+++ ecommerce_integrations/shopify/doctype/shopify_setting/shopify_setting.js
@@ -39,6 +39,20 @@ frappe.ui.form.on("Shopify Setting", {
 		});
 	},
 
+	sync_prices_now: function (frm) {
+		frappe.call({
+			doc: frm.doc,
+			method: "sync_prices_now",
+			freeze: true,
+			freeze_message: __("Queuing price sync to Shopify..."),
+			callback: (r) => {
+				if (!r.exc) {
+					frm.reload_doc();
+				}
+			},
+		});
+	},
+
 	refresh: function (frm) {
 		frm.add_custom_button(__("Import Products"), function () {
 			frappe.set_route("shopify-import-products");
